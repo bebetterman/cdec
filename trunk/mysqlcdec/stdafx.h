d0d0e8edcf98e8fa20b6970be59dca83b474fcdb
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-04 13:48:33 +0000

    新工程 mysqlcdec

---
+++ trunk/mysqlcdec/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "../include/cdeccore.h"
+
+#include "../import-win/mysqlconn-1.1.3/include/fake_boost.h"
+#include "../import-win/mysqlconn-1.1.3/include/mysql_connection.h"
+#include "../import-win/mysqlconn-1.1.3/include/mysql_driver.h"
+
+#include "../import-win/mysqlconn-1.1.3/include/cppconn/resultset.h"
+#include "../import-win/mysqlconn-1.1.3/include/cppconn/exception.h"
+#include "../import-win/mysqlconn-1.1.3/include/cppconn/statement.h"
+#include "../import-win/mysqlconn-1.1.3/include/cppconn/prepared_statement.h"
+
+using namespace std;
