1fe1c04ae60673068f1d0b69db453521ff3c042f
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-04 13:26:03 +0000

    Windows下mysql库

---
+++ trunk/import-win/mysqlconn-1.1.3/include/fake_boost.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace boost 
+{
+
+template<typename T1, typename T2 = unspecified, typename T3 = unspecified, typename T4 = unspecified>
+class variant
+{
+};
+
+template<typename T>
+class shared_ptr
+{
+};
+
+template<typename T>
+class scoped_ptr
+{
+};
+
+}
