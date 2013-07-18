2fc2869e1cb408889651946caf067d6564416949
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-18 14:02:51 +0000

    Post方法

--- trunk/httpserv/httpserv.cpp
+++ trunk/httpserv/httpserv.cpp
@@ -16,11 +16,6 @@ CDEC_NS_BEGIN
 // HandleContext
 // -------------------------------------------------------------------------- //
 
-stringx HandlerContext::GetArg(stringx key)
-{
-	return m_args[key];
-}
-
 int HandlerContext::SendResponse(UINT statusCode, const void* message, UINT length, bool fConstance)
 {
 	MHD_ResponseMemoryMode mm = fConstance ? MHD_RESPMEM_PERSISTENT : MHD_RESPMEM_MUST_COPY;
@@ -64,14 +59,24 @@ Server::Server(ref<IUrlDispatcher> dispatcher): m_daemon(NULL), m_dispatcher(dis
 
 void Server::Start(UINT port)
 {
-	UINT flags = MHD_USE_THREAD_PER_CONNECTION | MHD_USE_POLL;
-	// UINT flags = MHD_USE_THREAD_PER_CONNECTION | MHD_USE_POLL | MHD_USE_DEBUG;
+	UINT flags = MHD_USE_THREAD_PER_CONNECTION;
+#ifdef DEBUG_HTTPCDEC
+	flags |= MHD_USE_DEBUG;
+#endif
+#ifndef X_OS_WINDOWS
+	flags |= MHD_USE_POLL;
+#endif
+
 	MHD_Daemon* d;
-	d = MHD_start_daemon(flags, port, NULL, NULL, &ahc_echo, this, MHD_OPTION_END);
+	d = MHD_start_daemon(
+		flags, port, 
+		NULL, NULL, 
+		&OnRequestHandler, this,
+		MHD_OPTION_NOTIFY_COMPLETED, &OnRequestCompleted, this,
+		MHD_OPTION_END);
 	if (d == NULL)
 		cdec_throw(Exception(EC_Fail));
-	// getchar();
-	// MHD_stop_daemon(d);
+
 	while (true)
 	{
 		// Sleep for 10000 seconds
@@ -80,60 +85,143 @@ void Server::Start(UINT port)
 #else
 		sleep(10000);
 #endif
+	// MHD_stop_daemon(d);
 	}
 }
 
-int Server::parse_get_args(void *cls, enum MHD_ValueKind kind, const char *key, const char *value)
+int Server::OnParseGetArgs(void *cls, enum MHD_ValueKind kind, const char *key, const char *value)
 {
 	HandlerContext* ctx = (HandlerContext*)cls;
 	ref<Encoding> e = Encoding::get_UTF8();
 	stringx key_s = e->ToUnicode(key);
 	stringx value_s = value != NULL ? e->ToUnicode(value) : NULL;
-	ctx->m_args.insert(HandlerContext::Value(key_s, value_s));
+	ctx->m_getArgs.insert(HandlerContext::Value(key_s, value_s));
 	return MHD_YES;
 }
 
-int Server::ahc_echo(void* hdctx, MHD_Connection* connection, const char* url, const char* method, const char* version, const char* upload_data, size_t* upload_data_size, void** ptr)
+int Server::OnPostDataIterator(void *cls, enum MHD_ValueKind kind, const char *key, const char *filename, const char *content_type, const char *transfer_encoding, const char *data, uint64_t off, size_t size)
 {
-	static int dummy;
-	Server* server = (Server*)hdctx;
+	ASSERT(kind == MHD_POSTDATA_KIND);
+	ConnContext* conctx = (ConnContext*)cls;
+	HttpCdecDebugLog("[POST ITERATOR]");
+	HttpCdecDebugFormat("Key=%s\n", key);
+	if (filename != NULL)
+		HttpCdecDebugFormat("Filename=%s\n", filename);
+	if (content_type != NULL)
+		HttpCdecDebugFormat("MIME-type=%s\n", content_type);
+	if (transfer_encoding != NULL)
+		HttpCdecDebugFormat("Encoding=%s\n", transfer_encoding);
+	
+	std::string newvalue = std::string(data, size);
+	HttpCdecDebugFormat("Value=%s\n", newvalue.c_str());
 
-	if (&dummy != *ptr)
+	std::map<std::string, std::string>& map = conctx->KeyValueMap;
+	if (map.find(key) != map.end())
 	{
-		// The first time only the headers are valid, do not respond in the first round.
-		*ptr = &dummy;
-		return MHD_YES;
-	}
-
-	ref<HandlerContext> ctx = gc_new<HandlerContext>();
-	ctx->m_conn = connection;
-	ctx->m_url = Encoding::get_UTF8()->ToUnicode(url);
-
-	if (0 == strcmp(method, "GET"))
-	{
-		ctx->m_method = HandlerContext::HTTP_GET;
-		if (*upload_data_size != 0)
-			return MHD_NO;	// No upload data expected in HTTP GET method
-	}
-	else if (0 == strcmp(method, "POST"))
-	{
-		ctx->m_method = HandlerContext::HTTP_POST;
-		if (*upload_data_size == 0)
-			return MHD_NO;	// Upload data expected in HTTP POST method
+		std::string& value = map[key];
+		ASSERT(off == value.size());
+		value += newvalue;
 	}
 	else
-		return MHD_NO; // Unexpected method
+	{
+		ASSERT(off == 0);
+		map.insert(std::map<std::string, std::string>::value_type(key, newvalue));
+	}
 
-	// Parse GET arguments
-	MHD_get_connection_values(connection, MHD_GET_ARGUMENT_KIND, parse_get_args, ctx.__GetPointer());
+	return MHD_YES;
+}
 
-	// Parse headers
-	// MHD_get_connection_values(connection, MHD_HEADER_KIND, &print_out_key, NULL);
+int Server::OnRequestHandler(void* hdctx, MHD_Connection* connection, const char* url, const char* method, const char* version, const char* upload_data, size_t* upload_data_size, void** reqctx)
+{
+	Server* server = (Server*)hdctx;
 
-	*ptr = NULL; // clear context pointer
+	if (*reqctx == NULL)
+	{
+		ASSERT(upload_data == NULL && *upload_data_size == 0);
+		ConnContext* conctx = new ConnContext();
+		if (strcmp(method, MHD_HTTP_METHOD_GET) == 0)
+		{
+			conctx->Method = HTTPMETHOD_GET;
+			conctx->PostProcessor = NULL;
+		}
+		else if (strcmp(method, MHD_HTTP_METHOD_POST) == 0)
+		{
+			conctx->Method = HTTPMETHOD_POST;
+			conctx->PostProcessor = MHD_create_post_processor(connection, 1024, OnPostDataIterator, conctx);
+		}
+		else
+			return MHD_NO;
 
-	ref<IRequestHandler> handler = server->m_dispatcher->Dispatch(ctx->m_url);
-	return handler->Handle(ctx);
+		// The first time only the headers are valid, do not respond in the first round.
+		*reqctx = conctx;
+		return MHD_YES;
+	}
+	else
+	{
+		ref<HandlerContext> ctx = gc_new<HandlerContext>();
+		ctx->m_conn = connection;
+		ctx->m_url = Encoding::get_UTF8()->ToUnicode(url);
+
+		// Parse GET arguments
+		MHD_get_connection_values(connection, MHD_GET_ARGUMENT_KIND, OnParseGetArgs, ctx.__GetPointer());
+
+		// Parse headers
+		// MHD_get_connection_values(connection, MHD_HEADER_KIND, &print_out_key, NULL);
+
+		ref<IRequestHandler> handler = server->m_dispatcher->Dispatch(ctx->m_url);
+
+		ConnContext* conctx = (ConnContext*)(*reqctx);
+		if (conctx->Method == HTTPMETHOD_GET)
+		{
+			ASSERT(strcmp(method, MHD_HTTP_METHOD_GET) == 0);
+			ASSERT(*upload_data_size == 0);
+			ctx->m_method = HandlerContext::HTTP_GET;
+
+			// Do not call "*reqctx = NULL;" because a request completed callback would be called instead
+			return handler->Handle(ctx);
+		}
+		else if (conctx->Method == HTTPMETHOD_POST)
+		{
+			if (*upload_data_size != 0)
+			{
+				MHD_post_process (conctx->PostProcessor, upload_data, *upload_data_size);
+				*upload_data_size = 0;
+          
+				return MHD_YES;
+			}
+			else
+			{
+				HttpCdecDebugLog("[POST COMPLETE]");
+				ctx->m_method = HandlerContext::HTTP_POST;
+
+				// Do not call "*reqctx = NULL;" because a request completed callback would be called instead
+				return handler->Handle(ctx);
+			}
+		}
+		else
+			return MHD_NO;
+	}
+}
+
+void Server::OnRequestCompleted(void *cls, struct MHD_Connection *connection, void **con_cls, enum MHD_RequestTerminationCode toe)
+{
+	Server* server = (Server*)cls;
+	ConnContext* conctx = (ConnContext*)*con_cls;
+
+	if (conctx->PostProcessor != NULL)
+	{
+		puts("[POST REQUEST COMPLETED]");
+		ASSERT(conctx->Method == HTTPMETHOD_POST);
+		MHD_destroy_post_processor(conctx->PostProcessor);
+		conctx->KeyValueMap.clear();
+	}
+	else
+	{
+		ASSERT(conctx->Method != HTTPMETHOD_POST);
+	}
+
+	delete conctx;
+	*con_cls = NULL;   
 }
 
 // -------------------------------------------------------------------------- //
--- trunk/testers/TestHttpServer/TestHttpServer.cpp
+++ trunk/testers/TestHttpServer/TestHttpServer.cpp
@@ -29,12 +29,13 @@ class MyGetHandler: public IRequestHandler
 public:
 	int Handle(ref<HandlerContext> ctx)
 	{
+		ASSERT(ctx->GetMethod() == HandlerContext::HTTP_GET);
 		ref<StringBuilder> sb = gc_new<StringBuilder>();
 		sb->Append(__X("URL=") + ctx->GetUrl());
 		sb->Append('\n');
 
-		const HandlerContext::Values& args = ctx->GetArgs();
-		foreach_it (HandlerContext::Values::const_iterator, it, args.begin(), args.end())
+		const HandlerContext::StringPairMap& args = ctx->GetArgs();
+		foreach_it (HandlerContext::StringPairMap::const_iterator, it, args.begin(), args.end())
 		{
 			sb->Append(it->first);
 			sb->Append('=');
@@ -51,6 +52,35 @@ public:
 	}
 };
 
+class MyPostHandler: public IRequestHandler
+{
+	DECLARE_REF_CLASS(MyGetHandler)
+
+public:
+	int Handle(ref<HandlerContext> ctx)
+	{
+		ASSERT(ctx->GetMethod() == HandlerContext::HTTP_POST);
+		ref<StringBuilder> sb = gc_new<StringBuilder>();
+		sb->Append(__X("<html><body>\n<p>POST</p>\n"));
+		sb->Append(__X("<p>URL=") + ctx->GetUrl() + __X("</p>\n"));
+		sb->Append(__X("<ul>\n"));
+		/*
+		const HandlerContext::StringPairMap& args = ctx->PostArgs();
+		foreach_it (HandlerContext::StringPairMap::const_iterator, it, args.begin(), args.end())
+		{
+			sb->Append(__X("<li>"));
+			sb->Append(it->first);
+			sb->Append('=');
+			sb->Append(it->second != NULL ? it->second : __X("(null)"));
+			sb->Append(__X("</li>\n"));
+		}
+		*/
+		sb->Append(__X("</ul>\n</body></html>\n"));
+
+		return ctx->SendResponse(MHD_HTTP_OK, sb);
+	}
+};
+
 int main(int argc, const char* argv[])
 {
 	ref<DefaultUrlDispatcher> dispatcher = 
@@ -58,6 +88,7 @@ int main(int argc, const char* argv[])
 	dispatcher->Add(__X("/"), gc_new<StaticHandler>("Default Page"));
 	dispatcher->Add(__X("/hello"), gc_new<StaticHandler>("Hello Page"));
 	dispatcher->Add(__X("/get"), gc_new<MyGetHandler>());
+	dispatcher->Add(__X("/post"), gc_new<MyPostHandler>());
 
 	ref<Server> server = gc_new<Server>(dispatcher);
 	server->Start(6001);
