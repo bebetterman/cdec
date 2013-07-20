1bddcc6880701839b1115ad0208698fdf934b092
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-20 09:40:45 +0000

    Stream::Read/Write 参数调整，为避免隐藏错误，启用 Read2/Write2 方法名

--- cdeccore/io/filewrap.h
+++ cdeccore/io/filewrap.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#define USE_POSIX_FILEIO 1
-
 // -------------------------------------------------------------------------- //
 // File operation API wrapper
 //
@@ -33,10 +31,8 @@ private:
 
 #if defined(X_OS_WINDOWS)
 	HANDLE	m_hFile;
-#elif USE_POSIX_FILEIO
-	int		m_fd;
 #else
-	FILE*	m_fp;
+	int		m_fd;
 #endif
 
 public:
@@ -44,38 +40,33 @@ public:
 	{
 	#if defined(X_OS_WINDOWS)
 		m_hFile = INVALID_HANDLE_VALUE;
-	#elif USE_POSIX_FILEIO
-		m_fd = -1;
 	#else
-		m_fp = NULL;
+		m_fd = -1;
 	#endif
 	}
 
 	~FileWrapper() { Close(); }
 
-	bool IsOpen();
-	void Open(PCWSTR path, AccessMode access, ShareMode share, bool fCreate);
-	UINT Read(void* buffer, UINT cbToRead);
-	UINT Write(const void* buffer, DWORD cbToWrite);
-	INT64 Seek(INT64 offset, SeekOrigin origin);
-	INT64 Size();
-	void SetLength(INT64);
-	void Flush();
-	void Close();
-	UINT AtomRead(void* buffer, UINT cb,UINT64 pos);
-	UINT AtomWrite(const void* buffer, DWORD cb,UINT64 pos);
+	bool	IsOpen();
+	void	Open(PCWSTR path, AccessMode access, ShareMode share, bool fCreate);
+	int		Read(void* buffer, int cbToRead);
+	int		Write(const void* buffer, int cbToWrite);
+	int		AtomRead(INT64 pos, void* buffer, int cbToRead);
+	int		AtomWrite(INT64 pos, const void* buffer, int cbToWrite);
+	INT64	Seek(INT64 offset, SeekOrigin origin);
+	INT64	Size();
+	void	SetLength(INT64);
+	void	Flush();
+	void	Close();
 
 	// Inter-operation functions
 	// Do not use unless neccessary
 #if defined(X_OS_WINDOWS)
 	void FromHandle(HANDLE h);
 	HANDLE GetHandle() { return m_hFile; }
-#elif USE_POSIX_FILEIO
+#else
 	void FromFD(int fd);
 	int GetFD() { return m_fd; }
-#else
-	void FromFP(FILE* fp);
-	FILE* GetFP() { return m_fp; }
 #endif
 
 };
@@ -86,13 +77,9 @@ public:
 
 inline bool FileWrapper::IsOpen() { return m_hFile != INVALID_HANDLE_VALUE; }
 
-#elif USE_POSIX_FILEIO
-
-inline bool FileWrapper::IsOpen() { return m_fd != -1; }
-
 #else
 
-inline bool FileWrapper::IsOpen() { return m_fp != NULL; }
+inline bool FileWrapper::IsOpen() { return m_fd != -1; }
 
 #endif
 
