75c0d4a8b73a08418386f201ae275e638b0c0151
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-24 02:45:58 +0000

    Curl

--- curlcdec/stdafx.cpp
+++ curlcdec/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// CurlWrap.pch will be the pre-compiled header
+// curlcdec.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
