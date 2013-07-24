8150d5caab01fc07c07ab31404260f85860a3d41
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-24 04:02:41 +0000

    TestCurl

--- trunk/testers/TestCurl/TestCurl.cpp
+++ trunk/testers/TestCurl/TestCurl.cpp
@@ -1,9 +1,86 @@
-// TestCurl.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+
+// -------------------------------------------------------------------------- //
+
+#ifdef _WIN32
+#	ifdef _DEBUG
+#		pragma comment(lib, "libcurl_vc10_static_debug")
+#	endif
+#endif
+
+struct CurlException
+{
+	int code;
+	CurlException(int _code): code(_code) {}
+};
+
+#define MYURL	"http://localhost:6001/"
+
+#define VERIFY(f) UNITTEST_ASSERT((f) == 0)
+
+void Initialize()
+{
+#if defined(_WIN32)
+	long flags = CURL_GLOBAL_WIN32;	
+#else
+	long flags = CURL_GLOBAL_NOTHING;
+#endif
+
+	int code = curl_global_init(flags);
+	UNITTEST_ASSERT(code == 0);
+}
+
+void Terminate()
+{
+	curl_global_cleanup();
+}
+
+// -------------------------------------------------------------------------- //
+
+typedef std::string ByteBuffer;
+
+size_t CurlDataReceiveCallback(void *buffer, size_t size, size_t nmemb, void *user_p)
+{
+	ByteBuffer* p = (ByteBuffer*)user_p;
+	size_t length = size * nmemb;
+	p->append((const char*)buffer, size * nmemb);
+	return length;
+}
+
+// -------------------------------------------------------------------------- //
+
+void TestGet()
+{
+	puts("[TestGet]");
+
+	ByteBuffer buffer;
+
+	void* curl = curl_easy_init();
+	UNITTEST_ASSERT(curl != NULL);
+
+	VERIFY(curl_easy_setopt(curl, CURLOPT_URL, MYURL "get?name=abc&value=123"));
+	VERIFY(curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlDataReceiveCallback));
+	VERIFY(curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer));
+	VERIFY(curl_easy_perform(curl));
+
+	long responseCode = 0;
+	VERIFY(curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode));
+	UNITTEST_ASSERT(responseCode == 200);
+	
+	curl_easy_cleanup(curl);
+
+	puts(buffer.c_str());
+}
+
+// -------------------------------------------------------------------------- //
 
 int main(int argc, char* argv[])
 {
+	Initialize();
+
+	TestGet();
+
+	Terminate();
+
 	return 0;
 }
--- trunk/testers/TestCurl/stdafx.h
+++ trunk/testers/TestCurl/stdafx.h
@@ -5,4 +5,15 @@
 
 #pragma once
 
- 
+#ifdef _WIN32
+#	define CURL_STATICLIB
+#	include <Windows.h>
+#	include "../../import-win/curl-7.31.0/include/curl/curl.h"
+#else
+#	include <curl/curl.h>
+#endif
+
+#include <assert.h>
+#define UNITTEST_ASSERT(f)	assert(f)
+
+#include <string>
