47feeca351d07aac414ed2e6293e5d1bb5486063
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-26 11:19:29 +0000

    lzmacdec

---
+++ trunk/include/lzmacdec.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#ifndef __CDECCORE__
+#error cdeccore Expected
+#endif
+
+#ifndef __CDEC_LZMA__
+#define __CDEC_LZMA__
+#endif
+
+// -------------------------------------------------------------------------- //
+// Used in this project only 
+// -------------------------------------------------------------------------- //
+
+#ifdef LZMACDEC_EXPORTS
+#define LZMACDECEXPORT DECLSPEC_EXPORT
+#else
+#define LZMACDECEXPORT DECLSPEC_IMPORT
+#endif
+
+#ifdef X_OS_WINDOWS
+#define LZMACDECAPI(type)	EXTERN_C LZMACDECEXPORT type __stdcall
+#else
+#define LZMACDECAPI(type)	EXTERN_C LZMACDECEXPORT type
+#endif
+
+// -------------------------------------------------------------------------- //
+
+LZMACDECAPI(int) LzmaCdecCompress(
+	BYTE* dest, int& destLen, const BYTE* src, int srcLen,
+	BYTE* outProps, int& outPropsSize, /* *outPropsSize must be = 5 */
+	int level,      /* 0 <= level <= 9, default = 5 */
+	UINT dictSize,  /* default = (1 << 24) */
+	int lc,        /* 0 <= lc <= 8, default = 3  */
+	int lp,        /* 0 <= lp <= 4, default = 0  */
+	int pb,        /* 0 <= pb <= 4, default = 2  */
+	int fb,        /* 5 <= fb <= 273, default = 32 */
+	int numThreads /* 1 or 2, default = 2 */
+);
+
+LZMACDECAPI(int) LzmaCdecUncompress(
+	BYTE* dest, int& destLen, const BYTE* src, int& srcLen, const BYTE* props, int propsSize
+);
+
+// -------------------------------------------------------------------------- //
