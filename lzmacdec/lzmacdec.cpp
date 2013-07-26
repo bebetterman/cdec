1bd500eba639baac0a0eca7e19b07c0599a60238
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-26 11:19:29 +0000

    lzmacdec

---
+++ lzmacdec/lzmacdec.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "../include/lzmacdec.h"
+#include "lzmalib.h"
+
+// -------------------------------------------------------------------------- //
+// IMPORTANT
+//
+// Building lzmalib before this project.
+//
+// Under Windows
+//   Build project import-win\lzma920\C\Util\LzmaLib\LzmaLib.vcxproj (for Visual 
+// Studio 2010, supports for other versions shall be updated later).
+// -------------------------------------------------------------------------- //
+
+#ifdef X_OS_WINDOWS
+#pragma comment(lib, "cdecegg")
+#pragma comment(lib, "cdeccore")
+#pragma comment(lib, "LzmaLib")
+#endif
+
+// -------------------------------------------------------------------------- //
+
+LZMACDECAPI(int) LzmaCdecCompress(
+	BYTE* dest, int& destLen, const BYTE* src, int srcLen, BYTE* outProps, int& outPropsSize, 
+	int level, UINT dictSize, int lc, int lp, int pb, int fb, int numThreads
+	)
+{
+	size_t destLen_st = destLen;
+	size_t outPropsSize_st = outPropsSize;
+
+	int ret = LzmaCompress(
+		dest, &destLen_st, src, srcLen, outProps, &outPropsSize_st, 
+		level, dictSize, lc, lp, pb, fb, numThreads
+		);
+
+	destLen = (int)destLen_st;
+	outPropsSize = (int)outPropsSize_st;
+	return ret;
+}
+
+LZMACDECAPI(int) LzmaCdecUncompress(BYTE* dest, int& destLen, const BYTE* src, int& srcLen, const BYTE* props, int propsSize)
+{
+	size_t destLen_st = destLen;
+	size_t srcLen_st = srcLen;
+
+	int ret = LzmaUncompress(dest, &destLen_st, src, &srcLen_st, props, propsSize);
+
+	destLen = (int)destLen_st;
+	srcLen = (int)srcLen_st;
+	return ret;
+}
+
+// -------------------------------------------------------------------------- //
+
+#ifdef X_OS_WINDOWS
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+#endif
