8b0f2ac18e586d66569772d4827af8b5f3275bb7
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-07-29 06:05:12 +0000

    DateTime; 导出函数取掉 extern "C"，因为非 C++ 工程根本无法使用

--- trunk/include/cdeccore.h
+++ trunk/include/cdeccore.h
@@ -15,7 +15,7 @@
 #	define CDECCOREEXPORT		DECLSPEC_IMPORT
 #endif
 
-#define CDECCOREAPI(type)	EXTERN_C CDECCOREEXPORT type __stdcall
+#define CDECCOREAPI(type)		CDECCOREEXPORT type __stdcall
 
 // -------------------------------------------------------------------------- //
 // C-- Essential
