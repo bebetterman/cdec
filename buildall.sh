ef329612178f8076aceeea3d76904199187a42e3
yanggang1 <yanggang1@215ed966-f8df-9206-1724-2698b9f589ea>
2013-08-21 10:20:44 +0000

    buildall

---
+++ buildall.sh
@@ -0,0 +1,16 @@
+TYPE=Debug
+i=0
+len=${#proj[*]}
+
+if [ $1 -a $1 = "Release" ]; then 
+  TYPE=Release
+fi
+
+./build.sh cdecegg     $TYPE
+./build.sh cdeccore    $TYPE
+./build.sh zlibcdec    $TYPE
+./build.sh lzmacdec    $TYPE
+./build.sh mysqlcdec   $TYPE
+./build.sh curlcdec    $TYPE
+./build.sh httpserv    $TYPE
+./build.sh TestRunnerC $TYPE
