b4d6a316f786e0e9034e1c97719f4cc1891b5936
liyong0511@gmail.com <liyong0511@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-08-23 07:52:27 +0000

    修改格式化时间

--- trunk/cdeccore/common/datetime.cpp
+++ trunk/cdeccore/common/datetime.cpp
@@ -8,15 +8,20 @@ CDECCOREAPI(stringx) DateTime_Format(const DateTime& dt)
 	ref<StringBuilder> sb = gc_new<StringBuilder>();
 	sb->Append(Converter::ToString(dt.GetYear()));
 	sb->Append('-');
-	sb->Append(Converter::ToString(dt.GetMonth()));
+	stringx format = Converter::ToString(dt.GetMonth());
+	sb->Append(format.Length() == 2 ? format : __X("0") + format);
 	sb->Append('-');
-	sb->Append(Converter::ToString(dt.GetDay()));
+	format = Converter::ToString(dt.GetDay());
+	sb->Append(format.Length() == 2 ? format : __X("0") + format);
 	sb->Append(' ');
-	sb->Append(Converter::ToString(dt.GetHour()));
+	format = Converter::ToString(dt.GetHour());
+	sb->Append(format.Length() == 2 ? format : __X("0") + format);
 	sb->Append(':');
-	sb->Append(Converter::ToString(dt.GetMinute()));
+	format = Converter::ToString(dt.GetMinute());
+	sb->Append(format.Length() == 2 ? format : __X("0") + format);
 	sb->Append(':');
-	sb->Append(Converter::ToString(dt.GetSecond()));
+	format = Converter::ToString(dt.GetSecond());
+	sb->Append(format.Length() == 2 ? format : __X("0") + format);
 	return sb->ToString();
 }
 
