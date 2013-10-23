fa521ceaeaebc3f0b680ee712f5bd8dd3d08631f
yanggang1@gmail.com <yanggang1@gmail.com@215ed966-f8df-9206-1724-2698b9f589ea>
2013-10-23 12:07:51 +0000

    DateTime重构

--- cdeccore/common/datetime.cpp
+++ cdeccore/common/datetime.cpp
@@ -3,7 +3,137 @@
 CDEC_NS_BEGIN
 // -------------------------------------------------------------------------- //
 
-CDECCOREAPI(stringx) DateTime_Format(const DateTime& dt)
+void DateTime::Set(int year, int month, int day, int hour, int minute, int second, int milliseconds)
+{
+	DateTime dt;
+
+#ifdef X_OS_WINDOWS
+	dt.m_st.wYear = year;
+	dt.m_st.wMonth = month;
+	dt.m_st.wDay = day;
+	dt.m_st.wHour = hour;
+	dt.m_st.wMinute = minute;
+	dt.m_st.wSecond = second;
+	dt.m_st.wMilliseconds = milliseconds;
+
+	// todo: set wDayOfWeek
+#else
+	dt.m_tm.tm_year = year;
+	dt.m_tm.tm_mon = month;
+	dt.m_tm.tm_mday = day;
+	dt.m_tm.tm_hour = hour;
+	dt.m_tm.tm_min = minute;
+	dt.m_tm.tm_sec = second;
+	dt.m_milli = milliseconds;
+
+	// todo: set wDayOfWeek
+#endif
+
+	return dt;
+}
+
+void DateTime::SetLocal(INT64 timestamp, int milli)
+{
+#ifdef X_OS_WINDOWS
+	FILETIME ft, ftl;
+	(INT64&)ft = timestamp * 10000000 + 116444736000000000;
+
+	if (!FileTimeToLocalFileTime(&ft, &ftl))
+		cdec_throw_win32_lasterr(Exception);
+
+    SYSTEMTIME pst;
+	if (!FileTimeToSystemTime(&ftl, &m_st))
+		cdec_throw_win32_lasterr(Exception);
+#else
+	m_tm = *localtime(&timestamp);
+	m_milli = milli;
+#endif
+}
+
+void DateTime::SetUtc(INT64 timestamp, int milli)
+{
+#ifdef X_OS_WINDOWS
+	FILETIME ft;
+	(INT64&)ft = timestamp * 10000000 + 116444736000000000; 
+
+    SYSTEMTIME pst;
+	if (!FileTimeToSystemTime(&ft, &m_st))
+		cdec_throw_win32_lasterr(Exception);
+#else
+	m_tm = *gmtime(&timestamp);
+	m_milli = milli;
+#endif
+}
+
+INT64 DateTime::ToTimestamp()
+{
+#ifdef X_OS_WINDOWS
+	FILETIME ft;
+    if (!SystemTimeToFileTime(&m_st, &ft))
+		cdec_throw_win32_lasterr(Exception);
+ 
+	INT64 nll = (INT64&)ft;
+	return (nll - 116444736000000000) / 10000000;
+#else
+	cdec_throw(Exception(EC_NotImplemented));
+#endif
+}
+
+DateTime DateTime::Now()
+{
+#ifdef X_OS_WINDOWS
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+	return FromSystemTime(st);
+#else
+	timespec tmspec;
+	clock_gettime(CLOCK_REALTIME, &tmspec);
+	DateTime dt;
+	dt.SetTimeSpecLocal(tmspec);
+	return dt;
+#endif
+}
+
+DateTime DateTime::NowUtc()
+{
+#ifdef X_OS_WINDOWS
+	SYSTEMTIME st;
+	GetSystemTime(&st);
+	return FromSystemTime(st);
+#else
+	timespec tmspec;
+	clock_gettime(CLOCK_REALTIME, &tmspec);
+	DateTime dt;
+	dt.SetTimeSpecUtc(tmspec);
+	return dt;
+#endif
+}
+
+TimeValue DateTime::NowTimeValue()
+{
+#ifdef X_OS_WINDOWS
+	TimeValue tv;
+	__timeb64 tb;
+	_ftime64(&tb);
+	tv.Time = tb.time;
+	tv.Micro = tb.millitm * 1000;
+	tv.TimeZone = tb.timezone;
+	tv.DstFlag = tb.dstflag;
+	return tv;
+#else
+	TimeValue val;
+	struct timeval tv;
+	struct timezone tz;
+	gettimeofday(&tv, &tz);
+	val.Time = tv.tv_sec;
+	val.Micro = tv.tv_usec;
+	val.TimeZone = tz.tz_minuteswest;
+	val.DstFlag = tz.tz_dsttime;
+	return val;
+#endif
+}
+
+stringx DateTime_Format(const DateTime& dt)
 {
 	ref<StringBuilder> sb = gc_new<StringBuilder>();
 	sb->Append(Converter::ToString(dt.GetYear()));
@@ -25,11 +155,5 @@ CDECCOREAPI(stringx) DateTime_Format(const DateTime& dt)
 	return sb->ToString();
 }
 
-// -------------------------------------------------------------------------- //
-#ifdef X_OS_WINDOWS
-
-#else
-
-#endif
 // -------------------------------------------------------------------------- //
 CDEC_NS_END
--- include/cdeccore/common/datetime.h
+++ include/cdeccore/common/datetime.h
@@ -9,14 +9,6 @@
 CDEC_NS_BEGIN
 
 // -------------------------------------------------------------------------- //
-// Predeclaration, do not use
-// -------------------------------------------------------------------------- //
-
-struct DateTime;
-
-CDECCOREEXPORT stringx DateTime_Format(const DateTime& dt);
-
-// -------------------------------------------------------------------------- //
 // DateTime Value Type
 // -------------------------------------------------------------------------- //
 
@@ -35,7 +27,7 @@ struct TimeValue
 	INT16	DstFlag;	// DST flag (NOT tested)
 };
 
-struct DateTime
+struct CDECCOREEXPORT DateTime
 {
 protected:
 #ifdef X_OS_WINDOWS
@@ -46,147 +38,67 @@ protected:
 #endif
 
 public:
+	void	Set(int year, int month, int day, int hour, int minute, int second, int milliseconds);
+	void	SetLocal(INT64 timestamp, int milli);
+	void	SetUtc(INT64 timestamp, int milli);
+
+#ifdef X_OS_WINDOWS
+	inline void SetSystemTime(const SYSTEMTIME& st) { m_st = st; }
+	inline const SYSTEMTIME& ToSystemTime() const { return m_st; }
+#else
+	inline void	SetTimeSpecLocal(const timespec& ts) { SetLocal(ts.tv_sec, ts.tv_nsec / 1000000); }
+	inline void	SetTimeSpecUtc(const timespec& ts) { SetUtc(ts.tv_sec, ts.tv_nsec / 1000000); }
+#endif
+
+	INT64	ToTimestamp();
+
+	static	DateTime	Now();
+	static	DateTime	NowUtc();
+
 	// This will get the time-stamp, it is the same with time_t
 	// Return 64-bit time-stamp value under Windows 32-bit version
-	inline static INT64 Timestamp();
+	static	INT64		NowTimestamp();
 
 	// This will get the time-stamp, milli-second, time-zone, and DST information
 	// Refer timeb, ftime under Windows
 	// Refer
 	// Note that DST (day saving time) flags are NOT tested
-	inline static TimeValue NowTime();
-
-	inline static DateTime Now();
-
-	inline static DateTime Set(int year, int month, int day, int hour, int minute, int second, int milliseconds);
+	static	TimeValue	NowTimeValue();
 
 #ifdef X_OS_WINDOWS
-	inline static DateTime FromSystemTime(const SYSTEMTIME& st)
-	{
-		DateTime dt;
-		dt.m_st = st;
-		return dt;
-	}
+	inline int GetYear() const { return m_st.wYear; }
+	inline int GetMonth() const { return m_st.wMonth; }
+	inline int GetDay() const { return m_st.wDay; }
+	inline int GetHour() const { return m_st.wHour; }
+	inline int GetMinute() const { return m_st.wMinute; }
+	inline int GetSecond() const { return m_st.wSecond; }
+	inline int GetMilliseconds() const { return m_st.wMilliseconds; }
+	// inline int GetDayOfWeek() const { return m_st.wDayOfWeek; }
 #else
-	inline static DateTime FromTimeSpec(const timespec& ts)
-	{
-		DateTime dt;
-		dt.m_tm = *localtime(&ts.tv_sec);
-		dt.m_milli = ts.tv_nsec / 1000000;
-		return dt;
-	}
+	inline int GetYear() const { return m_tm.tm_year; }
+	inline int GetMonth() const { return m_tm.tm_mon; }
+	inline int GetDay() const { return m_tm.tm_mday; }
+	inline int GetHour() const { return m_tm.tm_hour; }
+	inline int GetMinute() const { return m_tm.tm_min; }
+	inline int GetSecond() const { return m_tm.tm_sec; }
+	inline int GetMilliseconds() const { return m_milli; }
+	// inline int GetDayOfWeek() const { return m_tm.tm_wday; }
 #endif
 
-#ifdef X_OS_WINDOWS
-	int	GetYear() const { return m_st.wYear; }
-	int GetMonth() const { return m_st.wMonth; }
-	int GetDay() const { return m_st.wDay; }
-	int GetHour() const { return m_st.wHour; }
-	int GetMinute() const { return m_st.wMinute; }
-	int GetSecond() const { return m_st.wSecond; }
-	int GetMilliseconds() const { return m_st.wMilliseconds; }
-	int GetDayOfWeek() const { return m_st.wDayOfWeek; }
-#else
-	int GetYear() const { return m_tm.tm_year; }
-	int GetMonth() const { return m_tm.tm_mon; }
-	int GetDay() const { return m_tm.tm_mday; }
-	int GetHour() const { return m_tm.tm_hour; }
-	int GetMinute() const { return m_tm.tm_min; }
-	int GetSecond() const { return m_tm.tm_sec; }
-	int GetMilliseconds() const { return m_milli; }
-	int GetDayOfWeek() const { return m_tm.tm_wday; }
-#endif
-
-	stringx	Format() { return DateTime_Format(*this); }
+	stringx	Format();
 };
 
 // -------------------------------------------------------------------------- //
+
+inline INT64 DateTime::NowTimestamp()
+{
 #ifdef X_OS_WINDOWS
-
-inline INT64 DateTime::Timestamp()
-{
 	return _time64(NULL);
-}
-
-inline TimeValue DateTime::NowTime()
-{
-	TimeValue tv;
-	__timeb64 tb;
-	_ftime64(&tb);
-	tv.Time = tb.time;
-	tv.Micro = tb.millitm * 1000;
-	tv.TimeZone = tb.timezone;
-	tv.DstFlag = tb.dstflag;
-	return tv;
-}
-
-inline DateTime DateTime::Now()
-{
-	SYSTEMTIME st;
-	GetLocalTime(&st);
-	return FromSystemTime(st);
-}
-
-inline DateTime DateTime::Set(int year, int month, int day, int hour, int minute, int second, int milliseconds)
-{
-	DateTime dt;
-	dt.m_st.wYear = year;
-	dt.m_st.wMonth = month;
-	dt.m_st.wDay = day;
-	dt.m_st.wHour = hour;
-	dt.m_st.wMinute = minute;
-	dt.m_st.wSecond = second;
-	dt.m_st.wMilliseconds = milliseconds;
-
-	// todo: set wDayOfWeek
-
-	return dt;
-}
-
 #else
-
-inline INT64 DateTime::Timestamp()
-{
 	return time(NULL);
-}
-
-inline TimeValue DateTime::NowTime()
-{
-	TimeValue val;
-	struct timeval tv;
-	struct timezone tz;
-	gettimeofday(&tv, &tz);
-	val.Time = tv.tv_sec;
-	val.Micro = tv.tv_usec;
-	val.TimeZone = tz.tz_minuteswest;
-	val.DstFlag = tz.tz_dsttime;
-	return val;
-}
-
-inline DateTime DateTime::Now()
-{
-	timespec tmspec;
-	clock_gettime(CLOCK_REALTIME, &tmspec);
-	return FromTimeSpec(tmspec);
-}
-
-inline DateTime DateTime::Set(int year, int month, int day, int hour, int minute, int second, int milliseconds)
-{
-	DateTime dt;
-	dt.m_tm.tm_year = year;
-	dt.m_tm.tm_mon = month;
-	dt.m_tm.tm_mday = day;
-	dt.m_tm.tm_hour = hour;
-	dt.m_tm.tm_min = minute;
-	dt.m_tm.tm_sec = second;
-	dt.m_milli = milliseconds;
-
-	// todo: set wDayOfWeek
-
-	return dt;
-}
-
 #endif
+}
+
 // -------------------------------------------------------------------------- //
 
 CDEC_NS_END
