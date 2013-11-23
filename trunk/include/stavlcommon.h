65dc560a6d6a881baafdf67f8f8a69b7b58da2a2
yanggang1 <yanggang1@215ed966-f8df-9206-1724-2698b9f589ea>
2013-11-23 07:11:13 +0000

    FastLogReader

---
+++ trunk/include/stavlcommon.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef __CDECCORE__
+#error cdeccore expected
+#endif
+
+#ifndef __STAVL_COMMON__
+#define __STAVL_COMMON__
+
+// -------------------------------------------------------------------------- //
+// Used in this project only 
+// -------------------------------------------------------------------------- //
+
+#ifdef STAVLCOMMON_EXPORTS
+#define STAVLCOMMONEXPORT DECLSPEC_EXPORT
+#else
+#define STAVLCOMMONEXPORT DECLSPEC_IMPORT
+#endif
+
+#include "stavlcommon/fastlogreader.h"
+
+#endif
--- trunk/stavlcommon/stdafx.h
+++ trunk/stavlcommon/stdafx.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "../include/cdeccore.h"
+#include "../include/stavlcommon.h"
