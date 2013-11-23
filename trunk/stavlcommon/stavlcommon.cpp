7de034fd8722c87db5f6e213bb8755bd8634ca43
yanggang1 <yanggang1@215ed966-f8df-9206-1724-2698b9f589ea>
2013-11-23 06:51:05 +0000

    新工程 stavlcommon

---
+++ trunk/stavlcommon/stavlcommon.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+
+// -------------------------------------------------------------------------- //
+// Config
+// -------------------------------------------------------------------------- //
+
+#ifdef X_OS_WINDOWS
+#	pragma comment(lib, "cdecegg.lib")
+#	pragma comment(lib, "cdeccore.lib")
+#endif
+
+bool cdec_PrintError = true;
+
+// -------------------------------------------------------------------------- //
+// Windows Only
+// -------------------------------------------------------------------------- //
+
+#ifdef X_OS_WINDOWS
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+#endif
+
+// -------------------------------------------------------------------------- //
