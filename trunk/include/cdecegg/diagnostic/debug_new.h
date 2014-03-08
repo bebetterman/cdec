f38e82bee0448818748a0092be01923b04185552
yanggang1 <yanggang1@215ed966-f8df-9206-1724-2698b9f589ea>
2014-03-08 11:17:51 +0000

    DebugNew 中间代码

--- trunk/include/cdecegg/diagnostic/debug_new.h
+++ trunk/include/cdecegg/diagnostic/debug_new.h
@@ -4,11 +4,36 @@
 
 #pragma once
 
-#define	Cdec_Enable_DebugNew	1
+#ifdef _DEBUG
+#	define	Cdec_Enable_DebugNew	1
+#else
+#	define	Cdec_Enable_DebugNew	0
+#endif
 
 // -------------------------------------------------------------------------- //
 
 #if Cdec_Enable_DebugNew
+
+CDECEGGEXPORT void* CdecDebugMalloc(size_t size, const char* file, int line);
+CDECEGGEXPORT void CdecDebugFree(void* p, const char* file, int line);
+
+// default operators
+inline void* operator new(size_t size) { return CdecDebugMalloc(size, NULL, 0); }
+inline void* operator new[](size_t size) { return CdecDebugMalloc(size, NULL, 0); }
+inline void operator delete(void* p) { CdecDebugFree(p, NULL, 0); }
+inline void operator delete[](void* p) { CdecDebugFree(p, NULL, 0); }
+
+// no-throw operators
+inline void* operator new(size_t size, const std::nothrow_t&) throw() { return CdecDebugMalloc(size, NULL, 0); }
+inline void* operator new[](size_t size, const std::nothrow_t&) throw() { return CdecDebugMalloc(size, NULL, 0); }
+inline void operator delete(void* p, const std::nothrow_t&) { CdecDebugFree(p, NULL, 0); }
+inline void operator delete[](void* p, const std::nothrow_t&) { CdecDebugFree(p, NULL, 0); }
+
+// placement operators
+inline void* operator new(size_t size, const char* file, int line) { return CdecDebugMalloc(size, file, line); }
+inline void* operator new[](size_t size, const char* file, int line) { return CdecDebugMalloc(size, file, line); }
+inline void operator delete(void* p, const char* file, int line) { CdecDebugFree(p, file, line); }
+inline void operator delete[](void* p, const char* file, int line) { CdecDebugFree(p, file, line); }
 
 #else
 
